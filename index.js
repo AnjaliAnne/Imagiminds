ab3d5da1b97646d48580a627557012df2a74e189
Amala-K-S <amalaks1999@gmail.com>
2020-09-25 16:43:25 +0530

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
+let ejs=require('ejs');
+
+app.set("view engine","ejs");
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin-amala:Smash1551@cluster0.yzaqe.mongodb.net/Imagiminds?retryWrites=true&w=majority";
@@ -19,6 +22,14 @@ app.get("/register",function(req,res){
     res.sendFile(path.join(__dirname,"/reg.html"));
 });
 
+app.get("/login",function(req,res){
+    res.sendFile(path.join(__dirname,"/login.html"))
+});
+
+app.get("/reset-password",function(req,res){
+    res.sendFile(path.join(__dirname,"/pass-reset.html"))
+});
+
 app.post("/register",function(req,res){
     var n=req.body.name;
     var e=req.body.email;
@@ -27,21 +38,58 @@ app.post("/register",function(req,res){
     var data={name:n,email:e,password:p1};
     if(p1==p2)
     {
-        db.collection('newusers').insertOne(data,function(err,r){
+        db.collection('newusers').findOne({email:{$eq:e}},function(err,r)
+        {
             if(err)
+                console.log(err);
+            else if(r==null)
             {
-                console.log("Cannot create an account");
-                res.sendFile(path.join(__dirname,"/reg.html"));
+                db.collection('newusers').insertOne(data,function(err,r){
+                    if(err)
+                    {
+                        console.log("Cannot create an account");
+                        res.sendFile(path.join(__dirname,"/reg.html"));
+                    }
+                    else
+                        res.render("userhomepage",{name:n});
+                });
             }
-            else
-                res.send("registration successful");
-        })
+            else{
+                res.send("Email already exists");
+            }
+        });
     }
     else    
         res.send("Passwords do not match");
-})
+});
 
+app.post("/login",function(req,res)
+{
+    var e=req.body.email;
+    var p=req.body.pass;   
+    db.collection('newusers').findOne({email:{$eq: e},password:{$eq:p}},function(err,r){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var name=r.name;
+            res.render("userhomepage",{name:name});
+        }
+    });
+});
 
+app.post("/reset-password",function(req,res)
+{
+    var e=req.body.email;
+    db.collection('newusers').findOne({email:{$eq:e}},function(err,r){
+        if(err)
+            console.log(err);
+        else
+        {
+
+        }
+    })
+}),
 
 app.listen( process.env.PORT || 3000,function(req,res){
     console.log("started");
--- public/scripts.js
+++ public/scripts.js
@@ -18,9 +18,65 @@ function passcheck()
 function fun1(){
     document.getElementById("msg").style.display="block";
   }
-  function fun2(){
+function fun2(){
     document.getElementById("msg").style.display="none";
+}
+
+function focusfun()
+{
+  document.getElementById("message").style.display="block";
+}
+function blurfun()
+{
+  document.getElementById("message").style.display="none";
+}
+
+function typefun()
+{
+  var pass=document.getElementById("p1");
+  var letter = document.getElementById("letter");
+  var capital = document.getElementById("capital");
+  var number = document.getElementById("number");
+  var length = document.getElementById("length");
+
+  var lowerCaseLetters = /[a-z]/g;
+  if(pass.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(pass.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
   }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(pass.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(pass.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
 
 function clickfun(){
     var x=document.getElementById("p1");
